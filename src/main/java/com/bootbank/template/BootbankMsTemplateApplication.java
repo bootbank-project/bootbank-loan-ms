251dae0b9bed9318a1cf2c123cae24e054969ce8
Huseyn Huseynov <huseynov.sys@gmail.com>
2026-06-17 15:16:14 +0400

    Initial commit

---
+++ src/main/java/com/bootbank/template/BootbankMsTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.bootbank.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootbankMsTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootbankMsTemplateApplication.class, args);
+	}
+
+}
