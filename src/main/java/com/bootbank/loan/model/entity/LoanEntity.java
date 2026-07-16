004dfcb998330e61cb9f0448317f068957bf6922
huseyn huseynov <004014@ibar.int>
2026-07-16 14:18:32 +0400

    inital refactorings

--- src/main/java/com/bootbank/loan/model/entity/LoanEntity.java
+++ src/main/java/com/bootbank/loan/model/entity/LoanEntity.java
@@ -14,28 +14,43 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Builder
 @Entity
-@Table(name = "loans", schema = "bootbank_loan_db")
+@Table(name = "LOAN")
 public class LoanEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "loan_seq")
+    @SequenceGenerator(name = "loan_seq", sequenceName = "LOAN_SEQ", allocationSize = 1)
     private Long id;
+    @Column(name = "CIF")
     private String cif;
+    @Column(name = "NAME")
     private String name;
     @Enumerated(EnumType.STRING)
+    @Column(name = "TYPE")
     private Type type;
+    @Column(name = "AMOUNT")
     private BigDecimal amount;
-    private Double rate;
+    @Column(name = "RATE")
+    private BigDecimal rate;
+    @Column(name = "MONTHLY_PAYMENT")
     private BigDecimal monthlyPayment;
     @Enumerated(EnumType.STRING)
+    @Column(name = "CURRENCY")
     private Currency currency;
+    @Column(name = "START_DATE")
     private LocalDate startDate;
+    @Column(name = "END_DATE")
     private LocalDate endDate;
     @Enumerated(EnumType.STRING)
+    @Column(name = "STATUS")
     private Status status;
+    @Column(name = "REMAINING")
     private BigDecimal remaining;
+    @Column(name = "TOTAL_PAID")
     private BigDecimal totalPaid;
+    @Column(name = "PAYMENTS_LEFT")
     private Integer paymentsLeft;
+    @Column(name = "NEXT_PAYMENT_DATE")
     private LocalDate nextPaymentDate;
 
 }
